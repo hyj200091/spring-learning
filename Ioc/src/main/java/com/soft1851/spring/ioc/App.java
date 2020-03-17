6533d17d631319b058b2dde788da40ca23ca2ffb
hyj <2298090131@qq.com>
2020-03-17 08:58:18 +0800

    spring-learning

---
+++ Ioc/src/main/java/com/soft1851/spring/ioc/App.java
@@ -0,0 +1,10 @@
+package com.soft1851.spring.ioc;
+
+/**
+ * @author hyj
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ Ioc/src/main/java/com/soft1851/spring/ioc/entity/Hello.java
@@ -0,0 +1,7 @@
+package com.soft1851.spring.ioc.entity;
+
+public class Hello {
+    public void sayHello(){
+        System.out.println("hello");
+    }
+}
